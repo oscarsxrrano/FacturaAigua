9c143d6ca3d19b520f2914d77cba612c3ecfd07c
oscarsxrrano <oscar.serrano.7e5@gmail.com>
2024-01-10 12:11:03 +0100

    Implementació de colors - Només queda kdoc

--- src/main/kotlin/Funcions.kt
+++ src/main/kotlin/Funcions.kt
@@ -1,10 +1,34 @@
 package org.example
 
 fun menuApp() {
-    println("  _____          _      _____ _    _ _               _____   ____  _____             _____ _____ _    _         \n" +
-            " / ____|   /\\   | |    / ____| |  | | |        /\\   |  __ \\ / __ \\|  __ \\      /\\   |_   _/ ____| |  | |  /\\    \n" +
-            "| |       /  \\  | |   | |    | |  | | |       /  \\  | |  | | |  | | |__) |    /  \\    | || |  __| |  | | /  \\   \n" +
-            "| |      / /\\ \\ | |   | |    | |  | | |      / /\\ \\ | |  | | |  | |  _  /    / /\\ \\   | || | |_ | |  | |/ /\\ \\  \n" +
-            "| |____ / ____ \\| |___| |____| |__| | |____ / ____ \\| |__| | |__| | | \\ \\   / ____ \\ _| || |__| | |__| / ____ \\ \n" +
-            " \\_____/_/    \\_\\______\\_____|\\____/|______/_/    \\_\\_____/ \\____/|_|  \\_\\ /_/    \\_\\_____\\_____|\\____/_/    \\_\\")
+    println(CYAN_BACKGROUND + WHITE_BOLD_BRIGHT +
+        "          _____ _____ _    _           __  __ ______ _   _ _    _ \n" +
+            "    /\\   |_   _/ ____| |  | |  /\\     |  \\/  |  ____| \\ | | |  | |\n" +
+            "   /  \\    | || |  __| |  | | /  \\    | \\  / | |__  |  \\| | |  | |\n" +
+            "  / /\\ \\   | || | |_ | |  | |/ /\\ \\   | |\\/| |  __| | . ` | |  | |\n" +
+            " / ____ \\ _| || |__| | |__| / ____ \\  | |  | | |____| |\\  | |__| |\n" +
+            "/_/    \\_\\_____\\_____|\\____/_/    \\_\\ |_|  |_|______|_| \\_|\\____/ ")
+
+    println(WHITE_BACKGROUND_BRIGHT + BLUE_BOLD +
+            "\nBenvingut al programa on podràs calcular quant has de pagar en aigua!")
 }
+
+fun resum(quotaFixa: Double,
+          precioPorLitro: Double,
+          litres: Int,
+          descuento: Int
+):String {
+
+    val quotaVariable = ((precioPorLitro*litres ) - ((precioPorLitro*litres)) * descuento / 100)
+    val preuFinal = quotaVariable + quotaFixa
+
+    val sortida:String = println(WHITE_BACKGROUND_BRIGHT + BLUE_BOLD +"\n" + BLACK_BACKGROUND + YELLOW_BOLD_BRIGHT +
+            "Amb aquestes dades, has de pagar $preuFinal€ al mes\n" +
+            "ESTADÍSTIQUES: " +
+            "Quota fixa: $quotaFixa\n" +
+            "Litres esgotats: $litres\n" +
+            "Preu per litre: $precioPorLitro\n" +
+            "Descompte: $descuento%").toString()
+    return sortida
+}
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,14 @@ package org.example
 import java.beans.beancontext.BeanContextChildComponentProxy
 
 fun main() {
+    do {
 
-    menuApp()
-    var quotaFixa = 6.0
-    var precioPorLitro = 0.0
-    val errorMSG1 = "Has de introduir un numero enter!"
+        menuApp()
+
+        var quotaFixa = 6.0
+        var precioPorLitro = 0.0
+        val errorMSG1 = "Has de introduir un numero enter!"
+        val errorMSG2 = "Has de introduir true/false"
 
 
         val litresMSG = ("Quants litres has gastat aquest mes?")
@@ -21,44 +24,47 @@ fun main() {
             precioPorLitro = 0.30
         }
 
+        val familiaMSG = ("Tens familia nombrosa o monoparental?")
+        val familia = readBoolean(familiaMSG, errorMSG2)
 
-    val errorMSG2 = "Has de introduir true/false"
-    val familiaMSG = ("Tens familia nombrosa o monoparental?")
-    val familia = readBoolean(familiaMSG, errorMSG2)
+        // Comprobem si la familia es monoparental o familia numerosa, i apliquem descomptes segons el numero de persones
+        var descuento = 10
+        if (familia) {
+            do {
+                val numeroFamMSG = ("Quants membres sou a la familia?")
+                val numFamilia = readInt(numeroFamMSG, errorMSG1)
 
-    // Comprobem si la familia es monoparental o familia numerosa, i apliquem descomptes segons el numero de persones
-    var descuento = 10
-    if (familia) {
-        do {
-            val numeroFamMSG = ("Quants membres sou a la familia?")
-            val numFamilia = readInt(numeroFamMSG, errorMSG1)
+                if (numFamilia in 1..5) {
+                    descuento *= numFamilia
+                }   else if (numFamilia > 5) {
+                    descuento *= 5
+                } else if (numFamilia == 0) {
+                    descuento = 0
+                    println("Mentiros!!!")
+                }
+            } while (numFamilia == 0)
+        } else {
+            println("No tens descompte")
+            descuento = 0
+        }
 
-            if (numFamilia in 1..5) {
-                descuento *= numFamilia
-            }   else if (numFamilia > 5) {
-                descuento *= 5
-            } else if (numFamilia == 0) {
-                descuento = 0
-                println("Mentiros!!!")
-            }
-        } while (numFamilia == 0)
-    } else {
-        println("No tens descompte")
-        descuento = 0
-    }
+        // COmprobem si el client te un bo social
 
-    // COmprobem si el client te un bo social
-    val boSocialMSG = ("Tens bo social?")
-    val boSocial = readBoolean(boSocialMSG, errorMSG2)
+        val boSocialMSG = ("Tens bo social?")
+        val boSocial = readBoolean(boSocialMSG, errorMSG2)
 
-    if (boSocial) {
-        quotaFixa = 3.0
-        descuento = 80
-    }
+        if (boSocial) {
+            quotaFixa = 3.0
+            descuento = 80
+        }
 
-    var quotaVariable = ((precioPorLitro*litres ) - ((precioPorLitro*litres)) * descuento / 100)
-    val preuFinal = quotaVariable + quotaFixa
 
-    println("$preuFinal€")
+        resum(quotaFixa, precioPorLitro, litres, descuento)
+
+        val calcul = readBoolean(WHITE_BACKGROUND_BRIGHT + BLUE_BOLD + "Vols tornar a començar?", errorMSG2)
+        if (!calcul) println("Gracies! Fins la propera!")
+
+    } while (calcul)
+
 
 }
