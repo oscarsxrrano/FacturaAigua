652e8b4351af7a4e2cd3b4431cdba813758c28e7
oscarsxrrano <oscar.serrano.7e5@itb.cat>
2024-01-11 23:18:50 +0100

    Final

--- src/main/kotlin/Funcions.kt
+++ src/main/kotlin/Funcions.kt
@@ -1,5 +1,11 @@
 package org.example
 
+/**
+ * Función para enseñar el menu por pantalla
+ * @author oscarsxrrano
+ * @since 2024-01-11
+ */
+
 fun menuApp() {
     println(CYAN_BACKGROUND + WHITE_BOLD_BRIGHT +
         "          _____ _____ _    _           __  __ ______ _   _ _    _ \n" +
@@ -13,6 +19,18 @@ fun menuApp() {
             "\nBenvingut al programa on podràs calcular quant has de pagar en aigua!")
 }
 
+/**
+ * Función para calcular el precio total
+ * Recoge los datos introducidos por el usuario anteriormente, y hace una operación para conseguir el precio del agua
+ * @param quotaFixa La quota fija a pagar
+ * @param precioPorLitro El precio por litro según lo gastado
+ * @param litres Los litros gastados por el cliente
+ * @param descuento El porcentaje de descuento según familia/bono
+ * @return Double: La suma entre 'quotaVariable' y 'quotaFija'
+ * @author oscarsxrrano
+ * @since 2024-01-11
+ */
+
 fun preuTotal(
     quotaFixa: Double,
     precioPorLitro: Double,
@@ -22,6 +40,19 @@ fun preuTotal(
     val quotaVariable = ((precioPorLitro * litres) - ((precioPorLitro * litres)) * descuento / 100)
     return quotaVariable + quotaFixa
 }
+
+/**
+ * Función para calcular el precio total
+ * Recoge los datos introducidos por el usuario anteriormente, y hace una operación para conseguir el precio del agua
+ * @param quotaFixa La quota fija a pagar
+ * @param precioPorLitro El precio por litro según lo gastado
+ * @param litres Los litros gastados por el cliente
+ * @param descuento El porcentaje de descuento según familia/bono
+ * @param preuFinal El total del calculo hecho em la funcion 'preuTotal'
+ * @return String: Estadisticas con todos los datos del cliente
+ * @author oscarsxrrano
+ * @since 2024-01-11
+ */
 
 fun resum(quotaFixa: Double,
           precioPorLitro: Double,
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -4,21 +4,15 @@ import org.junit.jupiter.api.Test
 
 class MainKtTest{
     @Test
-    fun checkString(){
-        val expected = "1.5"
-        assertEquals(expected, readInt("a", "a"))
-    }
-
-    @Test
-    fun checkBoolean(){
-        val expected = true
-        assertEquals(expected, readBoolean(pMessageIn = "tRUE", pMessageErrorDT = "Has de introduir true/false"))
-    }
-
-    @Test
     fun checkPreu(){ // quotaFixa: Double, precioPorLitro: Double, litres: Int, descuento: Int
 
-        val expected = 19.5
+        var expected = 19.5
         assertEquals(expected, preuTotal(6.0, 0.15, 90, 0))
+        expected = 21.0
+        assertEquals(expected, preuTotal(6.0, 0.15, 200, 50))
+        expected = 33.0
+        assertEquals(expected, preuTotal(3.0, 0.3, 500, 80))
+        expected = 1926.0
+        assertEquals(expected, preuTotal(6.0, 0.3, 8000, 20))
     }
 }
